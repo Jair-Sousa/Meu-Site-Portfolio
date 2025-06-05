b28acae3cbe17b3a78ded90b98dce85bd74d50a4
Jair Sousa <jairdomma@gmail.com>
2025-06-04 21:37:08 -0300

    commit-responsive1.1

--- scripts.js
+++ scripts.js
@@ -10,3 +10,13 @@ let header = document.getElementById('header');
 )
 
 
+// menu 
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const menuWrapper = document.querySelector(".menuamburguer");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  menuWrapper.classList.toggle("active");
+});
